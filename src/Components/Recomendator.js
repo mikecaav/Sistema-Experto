c5e4e156640d31e9494e20e3363384d7dc49a3cc
Miguel Caballero <caballero.mc98@gmail.com>
2021-06-24 19:57:52 -0500

    Made the app support images

--- src/Components/Recomendator.js
+++ src/Components/Recomendator.js
@@ -1,11 +1,11 @@
-import {Button, Container, Row, Col} from 'react-bootstrap'
-import { useEffect, useState } from "react"
+import {Button, Container, Row, Col, Card} from 'react-bootstrap'
+import { useEffect, useState} from "react"
 import {data} from "./data"
 
 export function Recomendator() {
     const defaultObject = {
         value: "",
-        isLeaf: false
+        isLeaf: false,
     }
     const [item, setItem] = useState(defaultObject)
     useEffect(()=>{
@@ -24,43 +24,50 @@ export function Recomendator() {
         const nextItemId = item[route]
         const nextItem = data["nodes"].filter(
             (actualItem)=>{
-                console.log(actualItem.id, nextItemId)
                 return actualItem.id === nextItemId
             }
         )[0]
-        console.log(nextItem)
         setItem(nextItem)
     }
-
+    console.log(process.env.PUBLIC_URL + item.logo)
 
     return(
         <Container>
-            <Row>
-                <h1 className="text-center mt-5">¡Recomiéndame un ánime!</h1>
-            </Row>
-            {
-                item.isLeaf ?
-                    <Row>
-                        <h3 className="text-center mt-5">Te recomendamos ver {item.value}</h3>
-                    </Row> :
-                    <Row>
-                        <h3 className="text-center mt-5">{item.value}</h3>
-                    </Row>
-            }
-            <Row>
+            <Card className="p-5 mt-5">
+                <Row>
+                    <h1 className="text-center mt-5">¡Recomiéndame un ánime!</h1>
+                </Row>
                 {
                     item.isLeaf ?
-                        null
-                        :
-                        <Col className="xs-12">
-                            <div className="text-center">
-                                <Button className="m-3" onClick={onClickButtonYes}>Sí</Button>
-                                <Button className="m-3" onClick={onClickButtonNo}>No</Button>
-                            </div>
-                        </Col>
+                        <Row>
+                            <h3 className="text-center mt-5">Te recomendamos ver {item.value}</h3>
+                        </Row> :
+                        <Row>
+                            <h3 className="text-center mt-5">{item.value}</h3>
+                        </Row>
                 }
+                <Row>
+                    {
+                        item.isLeaf ?
+                            null
+                            :
+                            <Col className="xs-12">
+                                <div className="text-center">
+                                    <Button className="m-3" onClick={onClickButtonYes}>Sí</Button>
+                                    <Button className="m-3" onClick={onClickButtonNo}>No</Button>
+                                </div>
+                            </Col>
+                    }
+                </Row>
+                {
+                    item.isLeaf ?
+                        <Row>
+                            <img src={item.logo} alt={item.logo} className="text-center p-5"/>
+                        </Row> :
+                        null
 
-            </Row>
+                }
+            </Card>
         </Container>
     )
 }
